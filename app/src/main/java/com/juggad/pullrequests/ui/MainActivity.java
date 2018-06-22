b54ff0d3d58113df53526898f91b0151ec859d73
Aman Jain <aman8sanky@gmail.com>
2018-06-22 17:48:41 +0530

    Adds comments

--- app/src/main/java/com/juggad/pullrequests/ui/MainActivity.java
+++ app/src/main/java/com/juggad/pullrequests/ui/MainActivity.java
@@ -52,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
         observeViewModel(pullRequestListViewModel);
     }
 
+    /*
+    Observe changes on live data
+     */
     private void observeViewModel(PullRequestListViewModel pullRequestListViewModel) {
         pullRequestListViewModel.getPullRequestListObservable().observe(this,
                 new Observer<Resource<List<PullRequestModel>>>() {
@@ -82,19 +85,26 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    private void showToast(final String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
-
+    /*
+    View initialization
+     */
     private void initView() {
         mProgressDialog = new ProgressDialog(this);
         mRepoNameEdit = findViewById(R.id.repository_name);
         mOwnerNameEdit = findViewById(R.id.owner_name);
         mRecyclerView = findViewById(R.id.recycler_view);
+        initializeRecyclerView();
+    }
+
+    /*
+    Initialize Recycler View
+     */
+    private void initializeRecyclerView() {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         pullRequestAdapter = new PullRequestAdapter(mPullRequestModels, new OnItemClickListener() {
             @Override
             public void onItemClicked(String url) {
+                //Open Pull Request in browser when clicked on any pull request from list
                 if (!url.startsWith("http://") && !url.startsWith("https://")) {
                     url = "http://" + url;
                 }
@@ -105,6 +115,9 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(pullRequestAdapter);
     }
 
+    /*
+    Method fetch data for open pull requests and hide keyboard.
+     */
     public void onSubmit(View v) {
         hideKeyboard(v);
         String repoName, ownerName;
@@ -118,12 +131,24 @@ public class MainActivity extends AppCompatActivity {
             showToast("Please Enter Owner Name");
             return;
         }
+        //Fetch open pull request for this particular owner/repo
         pullRequestListViewModel.fetchPullRequests(ownerName, repoName);
     }
 
+    /*
+    Utils method to hide keyboard
+     */
     public void hideKeyboard(View view) {
         InputMethodManager imm = (InputMethodManager) view.getContext()
                 .getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
+
+    /*
+    Utils method to show toast message
+     */
+    private void showToast(final String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
 }
