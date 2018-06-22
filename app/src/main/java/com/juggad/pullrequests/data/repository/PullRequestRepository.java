de051deb6319c93d5e4dcdd1bf6d61c9c4d49f4d
Aman Jain <aman8sanky@gmail.com>
2018-06-22 18:01:34 +0530

    Removed github token

--- app/src/main/java/com/juggad/pullrequests/data/repository/PullRequestRepository.java
+++ app/src/main/java/com/juggad/pullrequests/data/repository/PullRequestRepository.java
@@ -1,10 +1,6 @@
 package com.juggad.pullrequests.data.repository;
 
-import com.juggad.pullrequests.BuildConfig;
-import java.io.IOException;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -21,32 +17,12 @@ public class PullRequestRepository {
     private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
     private PullRequestRepository() {
-        httpClient.interceptors().add(getInterceptor());
-        OkHttpClient client = httpClient.build();
-
-        retrofit = new Retrofit.Builder().client(client)
+        retrofit = new Retrofit.Builder()
                 .baseUrl(PullRequestService.API_GITHUB_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         mPullRequestService = retrofit.create(PullRequestService.class);
-    }
-
-    private Interceptor getInterceptor() {
-        return new Interceptor() {
-            @Override
-            public okhttp3.Response intercept(Chain chain) throws IOException {
-                Request original = chain.request();
-
-                // Request customization: add request headers
-                Request.Builder requestBuilder = original.newBuilder()
-                        .header("Authorization", "token " + BuildConfig.GITHUB_TOKEN)
-                        .method(original.method(), original.body());
-
-                Request request = requestBuilder.build();
-                return chain.proceed(request);
-            }
-        };
     }
 
     public synchronized static PullRequestRepository getInstance() {
