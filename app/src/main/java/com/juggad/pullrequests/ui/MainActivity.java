4f06e03afeb75a1689f40da6355b65a21d2ab1df
Aman Jain <aman8sanky@gmail.com>
2018-06-22 17:23:23 +0530

    Removed data-binding

--- app/src/main/java/com/juggad/pullrequests/ui/MainActivity.java
+++ app/src/main/java/com/juggad/pullrequests/ui/MainActivity.java
@@ -1,25 +1,32 @@
 package com.juggad.pullrequests.ui;
 
+import android.app.ProgressDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.databinding.DataBindingUtil;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 import com.juggad.pullrequests.R;
 import com.juggad.pullrequests.data.model.PullRequestModel;
-import com.juggad.pullrequests.databinding.ActivityMainBinding;
+import com.juggad.pullrequests.ui.PullRequestAdapter.OnItemClickListener;
+import com.juggad.pullrequests.utils.Resource;
+import com.juggad.pullrequests.utils.Status;
 import com.juggad.pullrequests.viewmodel.PullRequestListViewModel;
-import com.juggad.pullrequests.viewmodel.PullRequestViewModelFactory;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    ActivityMainBinding mBinding;
 
     private RecyclerView mRecyclerView;
 
@@ -27,30 +34,96 @@ public class MainActivity extends AppCompatActivity {
 
     PullRequestAdapter pullRequestAdapter;
 
+    PullRequestListViewModel pullRequestListViewModel;
+
+    EditText mRepoNameEdit, mOwnerNameEdit;
+
+    ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        initRecyclerView();
+        setContentView(R.layout.activity_main);
+        initView();
 
-        PullRequestListViewModel pullRequestListViewModel = ViewModelProviders.of(this,
-                new PullRequestViewModelFactory(this.getApplication(), "hyperTrack", "hyperlog-android"))
+        pullRequestListViewModel = ViewModelProviders.of(this)
                 .get(PullRequestListViewModel.class);
 
-        pullRequestListViewModel.getPullRequestListObservable().observe(this, new Observer<List<PullRequestModel>>() {
-            @Override
-            public void onChanged(@Nullable final List<PullRequestModel> pullRequestModels) {
-                mPullRequestModels.clear();
-                mPullRequestModels.addAll(pullRequestModels);
-                pullRequestAdapter.notifyDataSetChanged();
-            }
-        });
+        observeViewModel(pullRequestListViewModel);
     }
 
-    private void initRecyclerView() {
-        mRecyclerView = mBinding.content.recyclerView;
+    private void observeViewModel(PullRequestListViewModel pullRequestListViewModel) {
+        pullRequestListViewModel.getPullRequestListObservable().observe(this,
+                new Observer<Resource<List<PullRequestModel>>>() {
+                    @Override
+                    public void onChanged(@Nullable final Resource<List<PullRequestModel>> listResource) {
+                        Status status = listResource.getStatus();
+                        if (Status.LOADING == status) {
+                            if (mProgressDialog != null) {
+                                mProgressDialog.setMessage("Fetching...");
+                                mProgressDialog.show();
+                            }
+                        } else if (Status.SUCCESS == status) {
+                            if (mProgressDialog != null) {
+                                mProgressDialog.cancel();
+                            }
+                            mPullRequestModels.clear();
+                            mPullRequestModels.addAll(listResource.getData());
+                            pullRequestAdapter.notifyDataSetChanged();
+                        } else if (Status.ERROR == status) {
+                            if (mProgressDialog != null) {
+                                mProgressDialog.cancel();
+                            }
+                            showToast(listResource.getMsg());
+                        }
+
+
+                    }
+                });
+    }
+
+    private void showToast(final String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void initView() {
+        mProgressDialog = new ProgressDialog(this);
+        mRepoNameEdit = findViewById(R.id.repository_name);
+        mOwnerNameEdit = findViewById(R.id.owner_name);
+        mRecyclerView = findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        pullRequestAdapter = new PullRequestAdapter(mPullRequestModels);
+        pullRequestAdapter = new PullRequestAdapter(mPullRequestModels, new OnItemClickListener() {
+            @Override
+            public void onItemClicked(String url) {
+                if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                    url = "http://" + url;
+                }
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
+            }
+        });
         mRecyclerView.setAdapter(pullRequestAdapter);
+    }
+
+    public void onSubmit(View v) {
+        hideKeyboard(v);
+        String repoName, ownerName;
+        repoName = mRepoNameEdit.getText().toString();
+        ownerName = mOwnerNameEdit.getText().toString();
+        if (TextUtils.isEmpty(repoName)) {
+            showToast("Please Enter Repository Name");
+            return;
+        }
+        if (TextUtils.isEmpty(ownerName)) {
+            showToast("Please Enter Owner Name");
+            return;
+        }
+        pullRequestListViewModel.fetchPullRequests(ownerName, repoName);
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
--- app/src/main/java/com/juggad/pullrequests/ui/PullRequestAdapter.java
+++ app/src/main/java/com/juggad/pullrequests/ui/PullRequestAdapter.java
@@ -1,13 +1,14 @@
 package com.juggad.pullrequests.ui;
 
-import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import com.juggad.pullrequests.R;
 import com.juggad.pullrequests.data.model.PullRequestModel;
-import com.juggad.pullrequests.databinding.PullRequestItemsBinding;
 import com.juggad.pullrequests.ui.PullRequestAdapter.PullRequestViewHolder;
 import java.util.List;
 
@@ -18,22 +19,38 @@ public class PullRequestAdapter extends RecyclerView.Adapter<PullRequestViewHold
 
     List<PullRequestModel> mPullRequestModels;
 
-    public PullRequestAdapter(final List<PullRequestModel> pullRequestModels) {
+    OnItemClickListener mOnItemClickListener;
+
+    public PullRequestAdapter(final List<PullRequestModel> pullRequestModels,
+            final OnItemClickListener onItemClickListener) {
         mPullRequestModels = pullRequestModels;
+        mOnItemClickListener = onItemClickListener;
     }
 
     @NonNull
     @Override
     public PullRequestViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        PullRequestItemsBinding binding =
-                DataBindingUtil.inflate(layoutInflater, R.layout.pull_request_items, parent, false);
-        return new PullRequestViewHolder(binding);
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.pull_request_items, parent, false);
+        return new PullRequestViewHolder(mView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull final PullRequestViewHolder holder, final int position) {
-        holder.mBinding.setData(mPullRequestModels.get(position));
+        final PullRequestModel data = mPullRequestModels.get(position);
+        holder.title.setText(String.format("#%s, %s", data.getNumber(), data.getTitle()));
+        holder.body.setText(data.getBody());
+        holder.createdAt.setText(data.getCreatedAt());
+        holder.updatedAt.setText(data.getUpdatedAt());
+        holder.name.setText(data.getUser().getLogin());
+        holder.itemView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                if (mOnItemClickListener != null) {
+                    mOnItemClickListener.onItemClicked(data.getUrl());
+                }
+            }
+        });
     }
 
     @Override
@@ -43,12 +60,22 @@ public class PullRequestAdapter extends RecyclerView.Adapter<PullRequestViewHold
 
     class PullRequestViewHolder extends RecyclerView.ViewHolder {
 
-        private final PullRequestItemsBinding mBinding;
+        TextView title, body, createdAt, updatedAt, name;
 
-        public PullRequestViewHolder(final PullRequestItemsBinding pullRequestItemsBinding) {
-            super(pullRequestItemsBinding.getRoot());
-            this.mBinding = pullRequestItemsBinding;
+        public PullRequestViewHolder(final View view) {
+            super(view);
+            title = view.findViewById(R.id.title);
+            body = view.findViewById(R.id.body);
+            createdAt = view.findViewById(R.id.created_at);
+            updatedAt = view.findViewById(R.id.updated_at);
+            name = view.findViewById(R.id.name);
+
         }
+    }
+
+    interface OnItemClickListener {
+
+        void onItemClicked(String url);
     }
 
 
